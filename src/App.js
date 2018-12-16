d315a382739dde3afe400675a96de0087fb45b0e
jsakowski <jsakowski@it.ucla.edu>
2018-12-16 09:26:16 +0000

    Completed

--- src/App.js
+++ src/App.js
@@ -106,6 +106,16 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		{
+        	profiles.map(profile => {
+          		const userName = users[profile.userID].name;
+				const movieName = movies[profile.favoriteMovieID].name;
+
+        		return (
+          			<li key={profile.id}>{userName}'s favorite movie is {movieName}</li>
+          		)
+        	})
+        }
       </div>
     );
   }
